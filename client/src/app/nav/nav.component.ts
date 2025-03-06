cb050c1f9fc8024e3cb6b581cb73aedc1b0390cb
tobezero01 <ducnhuad@gmail.com>
2025-03-07 05:11:27 +0700

    add login and register v1

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,7 +1,6 @@
 import { Component, inject } from '@angular/core';
 import {FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
-
 import {  BsDropdownModule } from 'ngx-bootstrap/dropdown';
 @Component({
   selector: 'app-nav',
@@ -12,14 +11,12 @@ import {  BsDropdownModule } from 'ngx-bootstrap/dropdown';
 })
 export class NavComponent {
   private accountService = inject(AccountService);
-  loggedIn = false;
   model: any = {};
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: response => {
         console.log(response);
-        this.loggedIn = true;
       },
       error : error => console.log(error)
     });
